b8d2e5b10fe0c9e4fc713abc640433d91b5cf5e4
Douglas Rogers <douglas.s.rogers.ctr@navy.mil>
2023-12-06 15:38:25 -0500

    WIP day 4 part 2

--- src/day/4/puzzle4.py
+++ src/day/4/puzzle4.py
@@ -6,13 +6,31 @@ def calcScore(wins: int):
     else:
         return 0
     
-def processLine(line: str):
-    input = line.split(':')[1].strip()
+class Game:
+    def __init__(self, cardNum, winningNums, drawnNums):
+        self.cardNum = cardNum
+        self.winningNums = winningNums
+        self.drawnNums = drawnNums
+
+    def getWins(self):
+        wins = 0
+        for drawnNum in self.drawnNums:
+            if drawnNum in self.winningNums:
+                wins += 1
+
+        return wins
+    
+def processLine(line: str, games: [], lines: []):
+    splitLine = line.split(':')
+    cardNum = splitLine.split()[1].strip()
+    input = splitLine[1].strip()
 
     splitInput = input.split('|')
 
     winningNums = splitInput[0].strip().split()
     drawnNums = splitInput[1].strip().split()
+
+    games.add(Game(cardNum, winningNums, drawnNums))
 
     wins = 0
     for drawnNum in drawnNums:
@@ -20,6 +38,29 @@ def processLine(line: str):
             wins += 1
 
     return wins
+
+def getGame(line: str):
+    splitLine = line.split(':')
+    cardNum = splitLine[0].split()[1].strip()
+    input = splitLine[1].strip()
+
+    splitInput = input.split('|')
+
+    winningNums = splitInput[0].strip().split()
+    drawnNums = splitInput[1].strip().split()
+
+    return Game(cardNum, winningNums, drawnNums)
+
+def processGame(game, games):
+    wins = game.getWins()
+    totalGames = [game]
+    if wins > 0:
+        nextGameIndex = int(game.cardNum)
+
+        for x in range(nextGameIndex, wins):
+            totalGames.extend(processGame(games[x], games))
+    
+    return totalGames
 
 def openFile(inputFileLoc):
     f = open(inputFileLoc, "r")
@@ -30,14 +71,23 @@ def main():
     # run puzzle
     file = openFile(inputFile)
 
-    totalScore = 0
+    lines = file.readlines()
 
-    for line in file:
-        wins = processLine(line)
-        totalScore += calcScore(wins)
+    games = []
+    for line in lines:
+        game = getGame(line)
 
-    result = totalScore
+        games.append(game)
+
+    totalGames = []
+
+    for game in games:
+        totalGames.extend(processGame(game, games.copy()))
+
+    result = len(totalGames)
     print(result)
+
+
 
 if __name__=="__main__": 
     main() 
