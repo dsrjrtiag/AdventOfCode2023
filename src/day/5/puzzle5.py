61f13da542390d3cdaba5117fcc3d885dff56e0c
Doug Rogers <douglas.rogers@tiag.net>
2023-12-07 23:31:37 -0500

    progress

--- src/day/5/puzzle5.py
+++ src/day/5/puzzle5.py
@@ -1,5 +1,21 @@
 inputFile = 'input/day/5/input.txt'
 
+class Ratio:
+    def __init__(self, dest, source, length):
+        self.dest = dest
+        self.source = source
+        self.length = length
+
+def processRatio(inputs, ratio : Ratio):
+    outputs = []
+    for input in inputs:
+        if input >= ratio.source and input < ratio.source + ratio.length:
+            outputs.append(input + (ratio.dest - ratio.source))
+        else:
+            outputs.append(input)
+
+    return outputs
+
 def openFile(inputFileLoc):
     f = open(inputFileLoc, "r")
 
@@ -8,20 +24,27 @@ def openFile(inputFileLoc):
 def readSeeds(line: str):
     seeds = []
 
-    seeds = line.split()[1:]
+    seeds = list(map(int, line.split()[1:]))
 
     return seeds
 
-def map(input: []):
-    return input
+def readRatio(line: str):
+    values = line.split()
+    return Ratio(int(values[0]), int(values[1]), int(values[2]))
+
 
 def main():
     # run puzzle
     file = openFile(inputFile)
 
-    seeds = readSeeds(file.readline())
+    outputs = readSeeds(file.readline())
 
-    result = seeds
+    for line in file:
+        if line[0].isdigit():
+            ratio = readRatio(line)
+            outputs = processRatio(outputs, ratio)
+    outputs.sort()
+    result = outputs
     print(result)
   
 if __name__=="__main__": 
